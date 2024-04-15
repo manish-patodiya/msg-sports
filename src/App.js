89e942826854d3d2cc3792621fbf623df1aad946
Manish Patodiya <mpatodiya2@gmail.com>
2024-04-15 12:08:41 +0530

    first push with tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Input } from '@material-tailwind/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Input label="Full Name" name="" className=''></Input>
+  )
 }
 
 export default App;
