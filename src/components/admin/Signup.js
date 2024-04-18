5b248d3b22282f195a426082cc107d9fdb10b770
Mahesh KM <Mahesh.Karupakula.Manjunath@msg-global.com>
2024-04-18 13:00:59 +0530

    New SIgnup page and added links in Login page

--- src/components/admin/Signup.js
+++ src/components/admin/Signup.js
@@ -9,7 +9,7 @@ import {
   Button,
 } from "@material-tailwind/react";
 
-const Login = () => {
+const Signup = () => {
   return (
     <div className="h-screen flex items-center justify-center px-10 py-10 bg-red-50">
       <Card className="w-96">
@@ -22,39 +22,21 @@ const Login = () => {
             </Typography>
           </div>
           <div>
-            <Input name="email" label="Email" size="lg" error={false} />
+            <Input name="email" label="Email" size="lg" error={false} placeholder="firtsname.lastname@msg-global.com" />
           </div>
           <div className="flex flex-col">
             <Input name="password" label="Password" size="lg" />
           </div>
+          <div className="flex flex-col">
+            <Input name="password" label="Confirm Password" size="lg" />
+          </div>
           <Button variant="" className="bg-rose-800 w-full">
-            Sign In
+            Sign Up
           </Button>
-          <Typography
-            variant="small"
-            className="mt-3 flex flex-col items-end justify-center"
-          >
-            <Typography
-              as="a"
-              href="/forgotPassword"
-              variant="small"
-              className="ml-1 mb-1 font-sans text-sm text-rose-800 underline"
-            >
-              Forgot Password?
-            </Typography>
-            <Typography
-              as="a"
-              href="/signup"
-              variant="small"
-              className="ml-1 mb-1 font-sans text-sm text-rose-800 underline"
-            >
-              New user?
-            </Typography>
-          </Typography>
         </CardBody>
       </Card>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Signup
