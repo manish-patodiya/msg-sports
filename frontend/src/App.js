8fed3fb58f0b942303d3f3e6e4e7bc6fbd757ce7
Manish Patodiya <mpatodiya2@gmail.com>
2024-04-27 15:40:26 +0530

    download toasty for showing toast notification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,22 @@ import "./App.css";
 import Admin from "./components/Admin";
 import Captain from "./components/Captains";
 import Player from "./components/Player";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Frontend />}></Route>
-        <Route path="/admin/*" element={<Admin />}></Route>
-        <Route path="/captain/*" element={<Captain />}></Route>
-        <Route path="/player/*" element={<Player />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Frontend />}></Route>
+          <Route path="/admin/*" element={<Admin />}></Route>
+          <Route path="/captain/*" element={<Captain />}></Route>
+          <Route path="/player/*" element={<Player />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
