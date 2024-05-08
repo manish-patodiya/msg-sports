6c41b79cc4da7c4b43bdd7cae5a8d581a2301615
Manish Patodiya <mpatodiya2@gmail.com>
2024-05-08 10:57:04 +0530

    show events at frontend side

--- frontend/src/components/frontend/Events.js
+++ frontend/src/components/frontend/Events.js
@@ -1,43 +1,64 @@
 import { Button, Card, CardBody, CardFooter, CardHeader, Typography } from '@material-tailwind/react'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { API_BASE_URL, BASE_URL } from '../../constants/constant'
+import { formatDateTime } from '../../common/common'
 
 const Events = () => {
-    let events = [
-        { img: "https://images.pexels.com/photos/3452356/pexels-photo-3452356.jpeg?auto=compress&cs=tinysrgb&w=600", name: "Cricket", desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi in distinctio minima ullam repellat expedita obcaecati dolore laborum sint enim eaque neque, at totam officia velit natus nisi tenetur et autem ratione eum, rem provident incidunt? Eaque, facilis enim saepe odit quaerat dolorum sed ex temporibus dicta quidem natus." },
-        { img: "https://images.pexels.com/photos/1089087/pexels-photo-1089087.jpeg?auto=compress&cs=tinysrgb&w=600", name: "Basketball", desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi in distinctio minima ullam repellat expedita obcaecati dolore laborum sint enim eaque neque, at totam officia velit natus nisi tenetur et autem ratione eum, rem provident incidunt? Eaque, facilis enim saepe odit quaerat dolorum sed ex temporibus dicta quidem natus." },
-        { img: "https://images.pexels.com/photos/5767580/pexels-photo-5767580.jpeg?auto=compress&cs=tinysrgb&w=600", name: "Badminton", desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum excepturi in distinctio minima ullam repellat expedita obcaecati dolore laborum sint enim eaque neque, at totam officia velit natus nisi tenetur et autem ratione eum, rem provident incidunt? Eaque, facilis enim saepe odit quaerat dolorum sed ex temporibus dicta quidem natus." }
-    ]
-    return (
-        <section id='events' className='mb-14'>
-            <Typography variant='h2' className='text-rose-800 font-sans'>Games & Events</Typography>
-            <div className='flex mt-8 gap-10 flex-col'>
-                {
-                    events.map((event, i) => {
-                        return <Card className='w-full' key={i}>
-                            <CardBody className={`flex gap-8 ${i % 2 != 0 || 'flex-row-reverse'}`}>
-                                <div className='w-1/2'>
-                                    <img
-                                        src={event.img}
-                                        alt={`Image ${i + 1}`}
-                                        className="max-h-[350px] w-full object-cover rounded-xl hover:-translate-y-1 hover:scale-105 duration-300"
-                                    />
-                                </div>
-                                <div className='w-1/2'>
-                                    <Typography variant='h3' className='mb-3'>{event.name}</Typography>
-                                    <Typography variant="h5" className="mb-2 font-serif font-thin">
-                                        {event.desc}
-                                    </Typography>
-                                    <Button className='bg-white text-rose-800 border-rose-800 shadow-none hover:shadow-none border mt-3'>See more</Button>
-                                </div>
+    const [events, setEvents] = useState([]);
+    useEffect(() => {
+        axios.get(API_BASE_URL + "events").then(res => {
+            if (res.data.status == 1) {
+                setEvents(res.data.response.events);
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [])
 
-                            </CardBody>
-                        </Card>
-                    })
-                }
+    if (events.length)
+        return (
+            <section id='events' className='mb-14'>
+                <Typography variant='h2' className='text-rose-800 font-sans'>Games & Events</Typography>
+                <div className='flex mt-8 gap-10 flex-col'>
+                    {
+                        events.map((event, i) => {
+                            return <Card className='w-full' key={i}>
+                                <CardBody className={`flex gap-8 ${i % 2 != 0 || 'flex-row-reverse'}`}>
+                                    <div className='w-1/2'>
+                                        <img
+                                            src={BASE_URL + "events/" + event.photo}
+                                            alt={`Image ${i + 1}`}
+                                            className="max-h-[350px] w-full object-cover rounded-xl hover:-translate-y-1 hover:scale-105 duration-300"
+                                        />
+                                    </div>
+                                    <div className='w-1/2 flex flex-col gap-3'>
+                                        <Typography variant='h4' className='text-rose-800'>{event.event_name}</Typography>
+                                        <div>
+                                            <Typography variant='h5' className=' text-gray-600'>{event.game_name}</Typography>
+                                            <Typography variant="paragraph" className="mb-2 font-thin">
+                                                {event.game_description}
+                                            </Typography>
+                                        </div>
+                                        <div className=''>
+                                            <Typography variant="small" className="mb-1 font-thin">
+                                                <span className='text-rose-800'>Date&Time: </span> {formatDateTime(event.date_time)}
+                                            </Typography>
+                                            <Typography variant="small" className="mb-2 font-thin ">
+                                                <span className='text-rose-800'>Venue: </span> {event.venue}
+                                            </Typography>
+                                        </div>
+                                        <Button size='sm' className='bg-white text-rose-800 border-rose-800 shadow-none hover:shadow-none border mt-3'>Register</Button>
+                                    </div>
 
-            </div>
-        </section>
-    )
+                                </CardBody>
+                            </Card>
+                        })
+                    }
+
+                </div>
+            </section>
+        )
 }
 
 export default Events
