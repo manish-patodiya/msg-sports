e6aa7a719cb299c735ceba6450d8c32cfda0f2ad
Manish Patodiya <mpatodiya2@gmail.com>
2024-05-16 21:16:17 +0530

    make dashboard for captain and player panel

--- frontend/src/components/captain/Dashboard.js
+++ frontend/src/components/captain/Dashboard.js
@@ -4,43 +4,48 @@ import React from "react";
 import { Chart } from "react-google-charts";
 const Dashboard = () => {
   const data = [
-    ["Player Status", "Status"],
-    ["Pending", 50],
-    ["Approved", 100],
-    ["rejected", 30]
+    ["Games", "count"],
+    ["Cricket", 14],
+    ["Football", 10],
+    ["Fooseball", 12],
+    ["Badminton", 8],
+    ["Box Cricket", 12],
+    ["Table Tennis", 22],
+    ["Chess", 4],
+    ["Carrom", 15]
   ];
 
   const options = {
-    title: "Player Registeration",
+    title: "Teams overview",
     is3D: true,
   };
   return (
     <div >
       <div className="flex gap-3">
-        <Card className="border w-1/4 h-28 bg-blue-900 text-white hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer">
+        <Card className="border w-1/4 h-28 bg-teal-500 text-white hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer">
           <CardBody>
-            <Typography>Team Members</Typography>
+            <Typography>House Members</Typography>
             <Typography>32</Typography>
           </CardBody>
         </Card>
-        <Card className="border w-1/4 h-28 bg-green-600 text-white hover:-translate-y-1 hover:scale-105 duration-300
+        <Card className="border w-1/4 h-28 bg-orange-500 text-white hover:-translate-y-1 hover:scale-105 duration-300
          cursor-pointer">
           <CardBody>
-            <Typography>Win</Typography>
-            <Typography>10</Typography>
+            <Typography>Wining Points</Typography>
+            <Typography>800 / 1000</Typography>
           </CardBody>
         </Card>
-        <Card className="border w-1/4 h-28 bg-pink-600 text-white hover:-translate-y-1 hover:scale-105 duration-300
+        <Card className="border w-1/4 h-28 bg-purple-600 text-white hover:-translate-y-1 hover:scale-105 duration-300
          cursor-pointer">
           <CardBody>
-            <Typography>Loose</Typography>
-            <Typography>6</Typography>
+            <Typography>Total Games</Typography>
+            <Typography>8</Typography>
           </CardBody>
         </Card>
       </div>
-      <div className="-ms-36">
+      <div className="-ms-12">
         <Chart
-          chartType="PieChart"
+          chartType="BarChart"
           data={data}
           options={options}
           width={"100%"}
