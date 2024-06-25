5de6ba1ee8592c993c64ab1d78a26247e83778f2
Shrushti Kaza <130393506+shrushtikaza@users.noreply.github.com>
2024-06-25 16:11:44 +0530

    adjusting padding

--- frontend/src/components/frontend/Results.js
+++ frontend/src/components/frontend/Results.js
@@ -1,7 +1,7 @@
 import { Card, CardHeader, CardBody, Typography, CardFooter, Button, Table } from '@material-tailwind/react'
 
 const tableData = {
-    headers: ["S.No", "Event", "Team 1 ID", "Team 2 ID", "Winner", "Points"],
+    headers: ["S.No", "Event", "Team 1", "Team 2", "Winner", "Points"],
     rows: [
         [1, "Men's Badminton - Singles", 1, 2, 1, 100],
         [2, "Women's Badminton - Singles", 3, 4, 3, 100],
@@ -15,26 +15,28 @@ const tableData = {
 const Results = () => { 
     return(
         <>
-         <div id="results" className="container mx-auto p-5">
+         <div id="results" className="container mx-auto">
             <Typography variant='h2' className='text-rose-800 font-sans'>Results</Typography>
-            <table className="border-collapse border border-gray-400 w-full">
-                <thead>
-                    <tr className="bg-rose-900 text-white">
-                        {tableData.headers.map((header, index) => (
-                            <th key={index} className="border border-gray-400 px-4 py-2">{header}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableData.rows.map((row, rowIndex) => (
-                        <tr key={rowIndex}>
-                            {row.map((cell, cellIndex) => (
-                                <td key={cellIndex} className="border border-gray-400 px-4 py-2">{cell}</td>
+            <div id='table1' className='p-5'>
+                <table className="border-collapse border border-gray-400 w-full">
+                    <thead>
+                        <tr className="bg-rose-900 text-white">
+                            {tableData.headers.map((header, index) => (
+                                <th key={index} className="border border-gray-400 px-4 py-2">{header}</th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {tableData.rows.map((row, rowIndex) => (
+                            <tr key={rowIndex}>
+                                {row.map((cell, cellIndex) => (
+                                    <td key={cellIndex} className="border border-gray-400 px-4 py-2">{cell}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
         </>
     )
