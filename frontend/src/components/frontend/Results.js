3c2f10632062e9bbdd563dfa8a8f5154021deaa9
Shrushti Kaza <130393506+shrushtikaza@users.noreply.github.com>
2024-06-25 16:00:46 +0530

    added results table

--- frontend/src/components/frontend/Results.js
+++ frontend/src/components/frontend/Results.js
@@ -1,11 +1,40 @@
 import { Card, CardHeader, CardBody, Typography, CardFooter, Button } from '@material-tailwind/react'
 
-const Results = () => {
+const tableData = {
+    headers: ["S.No", "Event", "Team 1 ID", "Team 2 ID", "Winner", "Points"],
+    rows: [
+        [1, "Men's Badminton - Singles", 1, 2, 1, 100],
+        [2, "Women's Badminton - Singles", 3, 4, 3, 100],
+        [3, "Foosball", 5, 2, 2, 200],
+        [4, "Carrom", 1, 2, 1, 100],
+        [5, "Men's Football", 3, 2, 2, 150],
+        [6, "Women's Cricket", 1, 4, 1, 100]
+    ]
+};
+
+const Results = () => { 
     return(
         <>
-        <Typography variant='h4' className='text-rose-800'>
-            this is the results section 
-        </Typography>
+         <div className="container mx-auto">
+            <table className="border-collapse border border-gray-400 w-full">
+                <thead>
+                    <tr className="bg-rose-900 text-white">
+                        {tableData.headers.map((header, index) => (
+                            <th key={index} className="border border-gray-400 px-4 py-2">{header}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.rows.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {row.map((cell, cellIndex) => (
+                                <td key={cellIndex} className="border border-gray-400 px-4 py-2">{cell}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
         </>
     )
 }
