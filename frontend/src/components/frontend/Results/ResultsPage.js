6c711a30884d64c3d391786ae73c64293cbc2698
Shrushti Kaza <130393506+shrushtikaza@users.noreply.github.com>
2024-06-26 14:48:11 +0530

    moved files

--- frontend/src/components/frontend/Results/ResultsPage.js
+++ frontend/src/components/frontend/Results/ResultsPage.js
@@ -1,11 +1,12 @@
 import { Card, CardHeader, CardBody, Typography, CardFooter, Button, Table } from '@material-tailwind/react'
 import React from "react";
-import Header from './Header';
+import Header from '../Header';
 import Results from './Results';
 
 const ResultsPage = () => {
     return(
         <>
+            
             <Typography className=" text-rose-900 variant='h2'"></Typography>
             <Results />
         </>
