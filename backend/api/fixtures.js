218b99003316ad41f960554c639519de5f20f783
Avya Kohli <avyakohli25@gmail.com>
2024-07-11 11:46:02 +0530

    Full Frontend and Backend completed for fixtures- Avya

--- backend/api/fixtures.js
+++ backend/api/fixtures.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getFixtures } from "../controller/fixtures.js";
+import { getNameTeamid1, getNameTeamid2, getNameWinner } from "../controller/fixtures.js";
+
 const router = express.Router();
 
-router.get("/:event_id", async (req, res) => {
-  res.json(await getFixtures(req.params.event_id));
+router.get("/:event_id/team1", async (req, res) => {
+  res.json(await getNameTeamid1(req.params.event_id));
+});
+
+router.get("/:event_id/team2", async (req, res) => {
+  res.json(await getNameTeamid2(req.params.event_id));
+});
+
+router.get("/:event_id/winner", async (req, res) => {
+  res.json(await getNameWinner(req.params.event_id));
 });
 
 export default router;
